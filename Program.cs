9c6fe82372b123516b7732747c2443cfb6abdc5c
eyunya11 <eyunya1126@icloud.com>
2025-12-28 21:33:13 +0900

    バグ修正

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using Discord;
+using Discord.WebSocket;
 using MCBot;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+{
+	GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+}));
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
