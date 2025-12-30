bd1bc5bf4caf120784e7e0bec6e654f3c2bd7a96
eyunya11 <eyunya1126@icloud.com>
2025-12-30 13:12:32 +0900

    依存関係の修正

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Discord.WebSocket;
 using MCBot;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+// IConfiguration と ILogger は自動的に登録される
 builder.Services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
 {
 	GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
